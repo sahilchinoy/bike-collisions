0d1dce8f90ea2d56351340b1afe6405293f5434e
Sahil Chinoy <sahil.chinoy@gmail.com>
2015-02-12 16:16:18 -0800

    Toggle between absolute/rate

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
-var maxIndex = 55;
+var maxIndex = 46;
+var maxNormalized = 3.66666666667;
 
 var map = L.map('map').setView([37.8612545, -122.2671221], 14);
 
@@ -10,15 +11,13 @@ L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 })
 .addTo(map);
 
-
-
 var radius = d3.scale.linear()
     .domain([0,100])
     .range([10,80]);
 
 var colorScale = d3.scale.quantize()
     .domain([0,100])
-.range(['rgb(255,255,178)','rgb(254,204,92)','rgb(253,141,60)','rgb(240,59,32)','rgb(189,0,38)']);
+    .range(['rgb(255,255,178)','rgb(254,204,92)','rgb(253,141,60)','rgb(240,59,32)','rgb(189,0,38)']);
 
 customCircle = L.Circle.extend({
    options: { 
@@ -27,6 +26,21 @@ customCircle = L.Circle.extend({
         streets: ''
    }
 });
+
+var Toggle = L.Control.extend({
+    options: {
+        position: 'topright'
+    },
+
+    onAdd: function (map) {
+        var container = L.DomUtil.create('div', 'toggle');
+        container.innerHTML = '<div class="btn-group" role="group"><button id="btn1" class="btn btn-sm btn-default active" onclick="changeDisplay(0)">Absolute</button> <button id="btn2" class="btn btn-sm btn-default" onclick="changeDisplay(1)">Rate</button></div>';
+        return container;
+    }
+});
+
+var toggle = new Toggle();
+toggle.addTo(map);
 
 var info = L.control();
 
@@ -38,32 +52,38 @@ info.onAdd = function (map) {
 
 info.update = function (options) {
     this._div.innerHTML = (options ?
-         '<b>' + options.streets + '</b> <br /> <b>' + options.index + '</b> danger index <br /><b>' + options.total + '</b> total accidents'
+         '<b>' + options.streets + '</b> <br /> <b>' + options.index + '</b> exposure index <br /><b>' + options.normalized + '</b> rate index <br /><b>' + options.bikes + '</b> bike accidents <br /><b>' + options.total + '</b> total accidents'
         : '<b>Hover over an intersection</b>');
 };
 
 info.addTo(map);
 
+var circle = new Array();
+
 for(var i = 0; i < incidents.length; i++) {
-    total = incidents[i].count;
-    index = incidents[i].danger;
-    quotient = (index/55 * 100).toFixed(1);
+    bikes = incidents[i].bikes;
+    total = incidents[i].total;
+    index = incidents[i].index;
+
+    quotient = (index/maxIndex * 100).toFixed(1);
+    normalized = ((incidents[i].normalized/maxNormalized) * 100).toFixed(1)
     
     coordinates = incidents[i].coordinates;
     
     var color = colorScale(quotient);
     var streets = incidents[i].streets;
     
-     new customCircle([coordinates[0], coordinates[1]], radius(quotient), {
+    circle[i] = new customCircle([coordinates[0], coordinates[1]], radius(quotient), {
         stroke: false,
         fillColor: color,
         fillOpacity: 0.8,
          
         index: quotient,
+        normalized: normalized,
+        bikes: bikes,
         total: total,
         streets: streets
     })
-    /*.bindPopup(streets + '</br> <strong>' + quotient + '</strong> danger index'+ '</br> <strong>' + total + '</strong>' + ' collisions')*/
     .on('mouseover', function(e) {
         info.update(e.target.options);})
     .on('click', function(e) {
@@ -71,6 +91,30 @@ for(var i = 0; i < incidents.length; i++) {
     .addTo(map)
 };
 
+function changeDisplay(type) {
+    for(var i = 0; i < circle.length; i++) {
+
+        if (type == 0) {
+            $('#btn1').addClass('active');
+            $('#btn2').removeClass('active');
+            index = circle[i].options.index;
+            rad = radius(index)
+            color = colorScale(index)
+        }
+
+        else {
+            $('#btn2').addClass('active');
+            $('#btn1').removeClass('active');
+            normalized = circle[i].options.normalized;
+            rad = radius(normalized)
+            color = colorScale(normalized)
+        }
+
+
+        circle[i].setRadius(rad);
+        circle[i].setStyle({fillColor: color});
+    }
+}
 
 var legend = L.control({position: 'bottomright'});
 
@@ -79,7 +123,7 @@ legend.onAdd = function (map) {
         grades = [0, 20, 40, 60, 80, 100],
         labels = [];
     
-    div.innerHTML = "<b>Danger index</b><br />";
+    div.innerHTML = "<b>Index scale</b><br />";
 
     for (var i = 0; i < 5; i++) {
         div.innerHTML +=
--- parser/parser.py
+++ parser/parser.py
@@ -7,147 +7,60 @@ myGeocoder = Geocoder(api_key="AIzaSyC0V4SJE-CLqvBKPWXu0b9iLwR_hmn9avo")
 outputfile = open('parsed_full.txt','w')
 partyfile = open('records.txt','r')
 
-collisionDictionary = {}
-totalsDictionary = {}
-dangerIndex = {}
-hourDictionary = {}
-weatherDictionary = {}
-streetsDictionary = {}
-yearDictionary = {}
-
-def hourizer(time):
-	if time >= 0 and time < 100:
-		return 0
-	elif time >= 100 and time < 200:
-		return 1
-	elif time >= 200 and time < 300:
-		return 2
-	elif time >= 300 and time < 400:
-		return 3
-	elif time >= 400 and time < 500:
-		return 4
-	elif time >= 500 and time < 600:
-		return 5
-	elif time >= 600 and time < 700:
-		return 6
-	elif time >= 700 and time < 800:
-		return 7
-	elif time >= 800 and time < 900:
-		return 8
-	elif time >= 900 and time < 1000:
-		return 9
-	elif time >= 1000 and time < 1100:
-		return 10
-	elif time >= 1100 and time < 1200:
-		return 11
-	elif time >= 1200 and time < 1300:
-		return 12
-	elif time >= 1300 and time < 1400:
-		return 13
-	elif time >= 1400 and time < 1500:
-		return 14
-	elif time >= 1500 and time < 1600:
-		return 15
-	elif time >= 1600 and time < 1700:
-		return 16
-	elif time >= 1700 and time < 1800:
-		return 17
-	elif time >= 1800 and time < 1900:
-		return 18
-	elif time >= 1900 and time < 2000:
-		return 19
-	elif time >= 2000 and time < 2100:
-		return 20
-	elif time >= 2100 and time < 2200:
-		return 21
-	elif time >= 2200 and time < 2300:
-		return 22
-	elif time >= 2300 and time < 2400:
-		return 23
-    
-    
-def parseSeason(month):
-    if month == 12 or month <= 2:
-        return 'AWinter'
-    elif month <=5:
-        return 'BSpring'
-    elif month <=8:
-        return 'CSummer'
-    else:
-        return 'DFall'
-
-def parseWeather(weather):
-	weatherDict = {'A': 'Clear', 'B': 'Cloudy', 'C': 'Raining', 'D': 'Snowing', 'E': 'Fog', 'F': 'Other', 'G': 'Wind', '-': 'N/A'}
-	return weatherDict[weather]
+totals = {}
+index = {}
+bikes = {}
+maxIndex = 0
+maxNormalized = 0
 
 def incidentScore(severity):
 	severityDictionary = {'1': 10, '2':5, '3':3, '4':2, '0':1}
 	return severityDictionary[severity]
 
 with open('records.txt', 'r') as read:
-    for line in read:
-        splitLine = line.split(",")
-        streets = [splitLine[3].replace("\"", ""), splitLine[4].replace("\"", "")]
-        streets.sort()
-        streets = tuple(streets)
+	for line in read:
+		splitLine = line.split(",")
+		streets = [splitLine[0].replace("\"", ""), splitLine[1].replace("\"", "")]
+		streets.sort()
+		streets = tuple(streets)
 
-        date = splitLine[0]
-        year = date[:4]
-        month = date[4:6]
-        ymonth = year + month
-        season = parseSeason(int(date[4:6]))
-        syear =   year + ' ' + str(season)
-        
-        time = int(splitLine[1])
-        hour = hourizer(time)
-        weekday = splitLine[2]
-        weather = parseWeather(splitLine[5])
-        severity = splitLine[6]
-        killed = splitLine[10]
-        injured = splitLine[11]
+		severity = splitLine[2]
 
-        if streets in collisionDictionary.keys():
-            collisionDictionary[streets].append((date, time, weekday))
-        else:
-            collisionDictionary[streets] = []
-            collisionDictionary[streets].append((date, time, weekday))
+		bike = False
 
-        if streets in totalsDictionary:
-            totalsDictionary[streets] += 1
-        else:
-            totalsDictionary[streets] = 1
+		if splitLine[3][0] == "Y":
+			bike = True
 
-        if hour in hourDictionary:
-            hourDictionary[hour] += 1
-        else:
-            hourDictionary[hour] = 1
+		if streets in totals:
+			totals[streets] += 1
+		else:
+			totals[streets] = 1
 
-        if month in yearDictionary:
-            yearDictionary[month] += 1
-        else:
-            yearDictionary[month] = 1
+		if bike:
+			if streets in bikes:
+				bikes[streets] += 1
+			else:
+				bikes[streets] = 1
 
-        if weather in weatherDictionary:
-            weatherDictionary[weather] += 1
-        else:
-            weatherDictionary[weather] = 1
+			if streets in index:
+				index[streets] += incidentScore(severity)
+			else:
+				index[streets] = incidentScore(severity)
 
-        if streets in dangerIndex:
-            dangerIndex[streets] += incidentScore(severity)
-        else:
-            dangerIndex[streets] = incidentScore(severity)
-            
-        
-ysorted = sorted(dangerIndex.items(), key=operator.itemgetter(1))
-
-for x in ysorted:
-    print(str(x[0]) + ',' + str(x[1]))
-
-"""for key in collisionDictionary.keys():
-	if totalsDictionary[key] > 1:
+for key in bikes.keys():
+	if index[key] > 10:
 		streets = key[0] + ' and ' + key[1]
-		print(streets)
 		coords = myGeocoder.geocode(streets + ", Berkeley CA").coordinates
+		
+		normalized = float(index[key])/float(totals[key])
+		if normalized > maxNormalized:
+			maxNormalized = normalized
 
-		outputfile.write(json.dumps({'streets': streets, 'coordinates': coords, 'count': totalsDictionary[key], 'danger': dangerIndex[key]}))
-		outputfile.write(",\n")"""
+		if index[key] > maxIndex:
+			maxIndex = index[key]
+
+		outputfile.write(json.dumps({'streets': streets, 'coordinates': coords, 'bikes': bikes[key], 'index': index[key], 'total': totals[key], 'normalized': normalized}))
+		outputfile.write(",\n")
+
+print("Maximum index  is " + str(maxIndex))
+print("Maximum normalized score is " + str(maxNormalized))
